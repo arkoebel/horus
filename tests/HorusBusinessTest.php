fd80734ac832a5c4d1065225a1ab0813399df6d0
Arnaud Koebel <arnaudkoebel@MacBook-Air-de-Arnaud.local>
2019-07-28 17:38:21 +0200

    Remove traces in test.

--- tests/HorusBusinessTest.php
+++ tests/HorusBusinessTest.php
@@ -11,14 +11,12 @@ class HorusBusinessTest extends TestCase {
     public function testFindMatch(): void{
         $horus = new HorusBusiness();
         $params = json_decode('[{"level1":"value1","level2":"value2","level3":{"key1":"value1","key2":"value2"}},{"level1":"value10","level2":"value20"},{"key3":"value3"}]',true);
-        var_dump($params);
-        print_r($params);
-        echo json_last_error_msg();
-        
         $this::assertEquals($horus->findMatch($params,0,"level1"),"value1");
         $this::assertEquals($horus->findMatch($params,1,"level2"),"value20");
         $this::assertEquals($horus->findMatch($params,0,"level3"),array("key1"=>"value1","key2"=>"value2"));
         $this::assertEquals($horus->findMatch($params,0,"levelX"),'');
         $this::assertEquals($horus->findMatch($params,5,"level1"),'');
     }
+
+    
 }

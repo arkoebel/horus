9bb79565fdbf4f51d5bfb5a3723d3d9f748956eb
arkoebel <koebela@yahoo.com>
2019-07-29 23:05:31 +0200

    Support logging inside business class. Test extra cases in locateJson

--- lib/horus_business.php
+++ lib/horus_business.php
@@ -3,6 +3,12 @@
 class HorusBusiness
 {
 
+    private $common = '';
+
+    function __construct($log_location){
+        $this->common = new HorusCommon($log_location);
+    }
+
     public function findMatch($matches, $request, $field)
     {
         if (array_key_exists($request, $matches)) {
@@ -16,7 +22,7 @@ class HorusBusiness
         }
     }
 
-    public function locate($matches, $found, $value)
+    public function locate($matches, $found, $value,$business_id)
     {
         $selected = -1;
         foreach ($matches as $id => $match) {
@@ -34,37 +40,48 @@ class HorusBusiness
         return $selected;
     }
 
-    function locateJson($matches, $input, $queryParams = null)
+    function locateJson($matches, $input, $queryParams = array(),$business_id=0)
     {
         $selected = -1;
+        if (is_null($input))
+            return $selected;
+        if (is_null($matches))
+            return $selected;
+        if (is_array($matches) && count($matches)==0)
+            return $selected;
+            if (is_array($input) && count($input)==0)
+            return $selected;
+
+        if (is_null($queryParams))
+            $queryParams = array();
         foreach ($matches as $id => $match) {
             if (array_key_exists($match['query']['key'], $input)) {
                 if (array_key_exists('queryKey', $match['query'])) {
                     if (array_key_exists($match['query']['queryKey'], $queryParams) && $match['query']['queryValue'] === $queryParams[$match['query']['queryKey']]) {
-                        mlog($id . ': trying -- Matched query param', 'DEBUG');
+                        $this->common->mlog($id . ': trying -- Matched query param', 'DEBUG','TXT','GREEN',$business_id);
                         if ($input[$match['query']['key']] === $match['query']['value']) {
                             if (array_key_exists('queryMatch', $match) && $match['queryMatch'] != '') {
                                 if (preg_match('/' . $match['queryMatch'] . '/', json_encode($input)) === 1) {
-                                    mlog($id . ': matched -- querymatch, query param', 'DEBUG');
+                                    $this->common->mlog($id . ': matched -- querymatch, query param', 'DEBUG','TXT','GREEN',$business_id);
                                     $selected = $id;
                                 }
                             } else {
-                                mlog($id . ': matched -- no query match, query param', 'DEBUG');
+                                $this->common->mlog($id . ': matched -- no query match, query param', 'DEBUG','TXT','GREEN',$business_id);
                                 $selected = $id;
                             }
                         }
                     } else {
-                        mlog($id . ': trying -- Query param wasn\'t a match', 'DEBUG');
+                        $this->common->mlog($id . ': trying -- Query param wasn\'t a match', 'DEBUG','TXT','GREEN',$business_id);
                     }
                 } else {
                     if ($input[$match['query']['key']] === $match['query']['value']) {
                         if (array_key_exists('queryMatch', $match) && $match['queryMatch'] != '') {
                             if (preg_match('/' . $match['queryMatch'] . '/', json_encode($input)) === 1) {
-                                mlog($id . ': matched -- querymatch, no query param', 'DEBUG');
+                                $this->common->mlog($id . ': matched -- querymatch, no query param', 'DEBUG','TXT','GREEN',$business_id);
                                 $selected = $id;
                             }
                         } else {
-                            mlog($id . ': matched -- no query match', 'DEBUG');
+                            $this->common->mlog($id . ': matched -- no query match', 'DEBUG','TXT','GREEN',$business_id);
                             $selected = $id;
                         }
                     }
